f9aa581f22a90c76667609787edab864d678638d
nn12vl9l <nn12vl9lsoft@gmail.com>
2021-10-31 11:37:28 +0900

    MemoApp完成

--- MemoApp/ViewController.swift
+++ MemoApp/ViewController.swift
@@ -7,13 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITextViewDelegate {
+    
+    @IBOutlet weak var textView: UITextView!
+    
+//    viewDidLoad読み込まれて表示されるまでの間に処理しておいてほしい内容
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textView.delegate = self
+        
+        let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as! SceneDelegate
+        textView.text = sceneDelegate.lastText
     }
-
+    
+//textviewでなにか変化が起きた時に処理する内容
+    func textViewDidChange(_ textView: UITextView) {
+        let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as! SceneDelegate
+        sceneDelegate.lastText = textView.text
+    }
 
 }
 
